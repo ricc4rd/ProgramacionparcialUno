e452c4fae61bdbb9966d09b7f7082d2493a6e41b
Ricardo Soto <menjivarsoto09@gmail.com>
2022-02-28 19:07:25 -0600

    primer parcial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <ApptoDo></ApptoDo>
+  </div>
+</template>
+
+<script>
+import ApptoDo from './components/ApptoDo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ApptoDo
+  }
+}
+</script>
+
+
